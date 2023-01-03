77b40f474911f79538040a6cf2424f1a66bc00f6
Roman-Huminilovych <103421237+Roman-Huminilovych@users.noreply.github.com>
2023-01-03 18:36:13 +0200

    c

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/portfolio-host">
     <App />
   </Router>,
   document.getElementById('root')
 );
-
