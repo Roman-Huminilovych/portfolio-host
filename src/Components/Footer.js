73233327345107e47fb507521cfceb3af2e8f242
Roman-Huminilovych <103421237+Roman-Huminilovych@users.noreply.github.com>
2023-03-27 14:40:22 +0300

    +

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,21 +3,21 @@ import Link from '@mui/material/Link';
 const Footer = () => {
   return (
     <Link
-      href="https://www.canva.com/design/DAFY3bJ2N1M/iPohBwdbsL8_PPRGDRz-fQ/view?utm_content=DAFY3bJ2N1M&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+      href="https://portfolio-developer-rh.netlify.app"
       target="_blank"
       rel="noopener noreferrer"
       sx={{
         width: '100vw',
         textAlign: 'center',
         position: 'absolute',
-        fontSize: '1rem',
+        fontSize: '0,9rem',
         left: 0,
-        bottom: 30,
+        bottom: 10,
         color: 'rgb(237, 245, 225, 0.5)',
         fontFamily: 'Montserrat',
       }}
     >
-      Переглянути презентацію
+      Created bt Roman Lovich
     </Link>
   );
 };
